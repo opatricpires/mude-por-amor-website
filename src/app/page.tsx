081cba1f74368b04518c96c32cd355ff516c94c6
f-ppires <f-ppires@jbfo.com>
2023-09-03 19:00:00 -0300

    feat: initial footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import HowToHelp from "@/components/HowToHelp";
 import Mission from "@/components/Mission";
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Mission />
       <HowToHelp />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="h-40 p-6 flex flex-col justify-center">
+      <p className="text-sm text-gray-900">
+        © 2023 Mude por amor, Todos os direitos reservados.
+      </p>
+      <div></div>
+    </footer>
+  );
+};
+
+export default Footer;
